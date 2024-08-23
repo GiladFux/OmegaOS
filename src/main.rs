a8d28a3c98edbea1863c9b6598c95e4c1962bc20
AlonNorani <alon28675@gmail.com>
2024-08-23 22:53:28 +0300

    fixed bug

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 #![no_std]
 #![no_main]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
 mod vga_buffer;
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     print!("Hello\tWorld{}", "!");
+    #[cfg(test)]
+    test_main();
     loop {}
 }
-
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
