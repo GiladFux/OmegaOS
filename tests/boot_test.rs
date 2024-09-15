2b3813915b08d56ff1fcb1dffedf95886e36acab
AlonNorani <alon28675@gmail.com>
2024-09-15 16:44:25 +0300

    moved tests to its own folder and added lib that contains all the important functions

--- tests/boot_test.rs
+++ tests/boot_test.rs
@@ -3,29 +3,20 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(omega::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
 use core::panic::PanicInfo;
 use omega::println;
-
-#[no_mangle]
+#[no_mangle] 
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     omega::test_panic_handler(info)
