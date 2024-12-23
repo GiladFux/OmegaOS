09e082990f3e5db28a407d7da40d688749111b0c
gilad <gilad@GiladComp.lan>
2024-12-23 19:24:20 -0100

    added initiallization of paging tables and turned on paging register

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -3,6 +3,8 @@ use crate::println;
 use crate::print;
 use lazy_static::lazy_static;
 use crate::gdt;
+use x86_64::structures::idt::PageFaultErrorCode;
+use crate::hlt_loop;
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -15,7 +17,9 @@ lazy_static! {
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
             idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+            idt.page_fault.set_handler_fn(page_fault_handler); 
 
+            
         idt
     };
 }
@@ -127,4 +131,16 @@ impl InterruptIndex {
     fn as_usize(self) -> usize {
         usize::from(self.as_u8())
     }
+}
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
